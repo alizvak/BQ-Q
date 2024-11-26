1171f59e7cc55e604a0ef7f5beed3c62f4ed8262
ali izadi <aliizadi27@gmail.com>
2024-11-26 23:02:18 +0330

    Compares the number of new and returning users each day
    Applications :
    - To track the overall health of your user base
    - To identify factors that may be affecting user retention

---
+++ daily_user_vs_new_user.sql
@@ -0,0 +1,31 @@
+-- Declare date range variables for filtering
+DECLARE start_date STRING DEFAULT '2024-01-01';
+DECLARE end_date STRING DEFAULT '2024-12-31';
+
+WITH user_sessions AS (
+    -- Extract user session data including session ID and session number
+    SELECT
+        user_pseudo_id,
+        (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS session_id,
+        (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_number') AS session_number
+    FROM
+        `ga4-janebi.analytics_307705149.events_*`
+    WHERE 
+        _TABLE_SUFFIX BETWEEN REPLACE(start_date, '-', '') AND REPLACE(end_date, '-', '') -- Date range filter
+),
+
+user_type_counts AS (
+    -- Count the number of returning users (session number > 1) and new users (session number = 1)
+    SELECT 
+        COUNT(DISTINCT IF(session_number > 1, user_pseudo_id, NULL)) AS returning_user_count,
+        COUNT(DISTINCT IF(session_number = 1, user_pseudo_id, NULL)) AS new_user_count
+    FROM 
+        user_sessions
+)
+
+-- Output the user counts
+SELECT 
+    returning_user_count AS returning,
+    new_user_count AS new_user
+FROM 
+    user_type_counts;
